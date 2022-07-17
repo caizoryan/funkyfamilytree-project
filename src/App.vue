74f6d849afa62bcc077071507ff829a58ed75fce
Aaryan Pashine <pashineaaryan@gmail.com>
2022-07-17 22:07:42 +0530

    Added mouse, line, images and scalability

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,76 @@
 <template>
-<HelloWorld
-:top=ar.top
-:left=ar.left
-:bottom=ar.top
-:right=ar.left
+<div id="p5Canvas"></div>
+<HelloWorld v-for="arr in ar"
+:key=arr.id
+:image-y=arr.top
+:image-x=arr.left
+:label-y=arr.top
+:label-x=arr.left
+:image1=arr.image1
+:image2=arr.image2
+:label=arr.label
+:id=arr.id
 />
+
+<div id="cursor" :style="{
+  transform: 'translate(' + valuex + 'px,' + valuey + 'px)'
+}"><img src="@/assets/cursor.png" alt=""></div>
+
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import P5 from 'p5'
+import { ref, reactive, onBeforeMount, onMounted } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
-const ar = reactive({ id: 0, top: Math.random() * window.innerHeight, left: Math.random() * window.innerWidth })
 
+const ar = reactive(
+  [{ id: 0, label: 'Armaan', top: Math.random() * window.innerHeight, left: Math.random() * window.innerWidth, image1: require('@/assets/1/1.jpeg'), image2: require('@/assets/1/2.jpeg') },
+    { id: 1, label: 'Aaryan', top: 200, left: 900, image1: require('@/assets/2/1.jpeg'), image2: require('@/assets/2/2.jpeg') }
+  ])
+
+const sketch = (s) => {
+  s.setup = () => {
+    s.createCanvas(window.innerWidth, window.innerHeight)
+  }
+  s.draw = () => {
+    s.background(200)
+    s.strokeWeight(1)
+    s.line(s.mouseX, s.mouseY, window.innerWidth - s.mouseX, window.innerHeight - s.mouseY)
+    s.fill(200)
+    s.strokeWeight(0)
+    s.circle(s.mouseX, s.mouseY, 50)
+    s.circle(window.innerWidth - s.mouseX, window.innerHeight - s.mouseY, 50)
+  }
+}
+
+let d
+
+onBeforeMount(() => {
+  d = new P5(sketch, 'p5Canvas')
+})
+
+onMounted(() => {
+  window.addEventListener('mousemove', update)
+  d.background('red')
+})
+const valuex = ref(0)
+const valuey = ref(0)
+function update (event) {
+  valuex.value = window.innerWidth - event.pageX
+  valuey.value = window.innerHeight - event.pageY
+}
 </script>
 
 <style lang="scss">
+
+#p5Canvas{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +78,20 @@ const ar = reactive({ id: 0, top: Math.random() * window.innerHeight, left: Math
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#cursor{
+  width: 25px;
+  height: 25px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  img{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,64 @@
 <template>
 <div class="image-box" v-bind:style="{
-  top: props.top + 'px',
-  left: props.left + 'px'
-}"></div>
+  left: props.imageX + 'px',
+  top: props.imageY + 'px'
+}" @mouseover="hover = true" @mouseleave="hover = false">
 
-<div class="opposite" v-bind:style="{
-  bottom: props.bottom + 'px',
-  right: props.right + 'px'
-}"></div>
+<img v-if="hover == false" :src=props.image1 alt="">
+<img v-if="hover" :src=props.image2 alt="">
+
+</div>
+
+<div class="label" v-bind:style="{
+  right: props.labelX - 25 + 'px',
+  bottom: props.labelY - 25 + 'px'
+}" @mouseover="hover = true" @mouseleave="hover = false">
+<h1>{{ props.label }}</h1>
+</div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, onMounted, ref } from 'vue'
+const hover = ref(false)
+const props = defineProps(['imageX', 'imageY', 'labelX', 'labelY', 'image1', 'image2', 'label', 'id'])
 
-const props = defineProps(['top', 'left', 'bottom', 'right'])
+onMounted(() => {
+  const id = props.id
+  console.log(id)
+})
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
 .image-box{
-  width: 200px;
-  height: 200px;
-  background-color: red;
+  width: 100px;
+  height: 100px;
+  background-color: rgb(177, 177, 177);
   position: absolute;
+  img{
+    position: absolute;
+    top: 7.5%;
+    left: 7.5%;
+    width: 85%;
+    height: 85%;
+  }
 }
 
-.opposite{
-  width: 200px;
-  height: 200px;
+.label{
+  width: 100px;
+  height: 100px;
   position: absolute;
-  background-color: blue;
-
+  background-color: rgb(200, 200, 200);
+  border: 2px solid black;
+  display: flex;
+  // justify-content: center;
+  // align-items: center;
+  h1{
+    padding-left: 10px;
+    color: black;
+    font-size: 15px;
+  }
 }
 
 h3 {
